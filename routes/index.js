b0245701de04a8fc595b24d54dbb159aac65d586
cuongnt45 <cuongnt45@viettel.com.vn>
2022-04-19 09:04:56 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,19 @@ const  db ="mongodb+srv://khactuan2312:khactuan@cluster0.dhfhw.mongodb.net/ontap
 router.use(bodyParser.urlencoded({
   extended: true
 }))
+
 mongoose.connect(db).catch(error => {
   console.log("co loi xay ra" + error);
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  xe.find({}, function (err, data) {
+    if (err == null) {
+      res.render('list_xe', {title: 'ListAll', data: data});
+    } else {
+      console.log(err.message);
+    }
+  })
 });
 /* UPLOAD FILE*/
 var nameImage = "";
@@ -38,7 +45,7 @@ function fileFilter(req, file, cb) {
 }
 // khai bao Schema
 const xeModel = new mongoose.Schema({
-  _id: "string", _gia: "string", _anh: "string", _soluong: "string"
+  _id: "string", _gia: "string", _image: "Array", _soluong: "string"
 });
 const xe = mongoose.model("ontap2", xeModel);
 var upload = multer({storage: storage, fileFilter: fileFilter});
@@ -47,7 +54,7 @@ var upload = multer({storage: storage, fileFilter: fileFilter});
 router.get('/List', function (req, res, next) {
   xe.find({}, function (err, data) {
     if (err == null) {
-      res.render('List', {title: 'ListAll', data: data});
+      res.render('list_xe', {title: 'ListAll', data: data});
     } else {
       console.log(err.message);
     }
@@ -60,12 +67,12 @@ router.get('/update', function (req, res, next) {
 });
 
 router.post("/Update", upload.array("profile_pic", 2), function (req, res,) {
-  let _id = req.body.id;
-  let _email = req.body.email;
-  let _diaChi = req.body.diaChi;
-  let _khoa = req.body.khoa;
+  let _id = req.body._id;
+  let _gia = req.body._gia;
+  let _soluong = req.body._soluong;
   let _file = req.files;
   let _namefile = [];
+  console.log(req.body, 'req update')
   //
   // if (!_id) {
   //     const err = new Error("Chua nhap id!");
@@ -86,8 +93,9 @@ router.post("/Update", upload.array("profile_pic", 2), function (req, res,) {
   for (let i = 0; i < _file.length; i++) {
     _namefile.push(_file[i].filename);
   }
-  student.updateOne({_id: _id}, {_email: _email, _diaChi: _diaChi, _khoa: _khoa, _image: _namefile}, function (err) {
+  xe.updateOne({_id: _id}, {_gia: _gia, _soluong: _soluong, _image: _namefile}, function (err) {
     if (err == null) {
+      console.log("update thanh cong")
       res.redirect("/List");
     } else {
       res.send(err.message);
@@ -98,7 +106,7 @@ router.post("/Update", upload.array("profile_pic", 2), function (req, res,) {
 })
 
 router.post("/getXe", function (req, res,) {
-  let _id = req.body.id;
+  let _id = req.body._id;
   xe.findOne({_id: _id}, function (err, data) {
     if (err == null) {
       res.send({
@@ -120,7 +128,7 @@ router.get('/delete', function (req, res, next) {
 });
 
 router.post("/Delete", function (req, res,) {
-  let _id = req.body.id;
+  let _id = req.body._id;
   xe.deleteOne({_id: _id}, function (err) {
     if (err == null) {
       res.send({
@@ -140,23 +148,21 @@ router.post("/Delete", function (req, res,) {
 
 /* GET add page. */
 router.get('/add', function (req, res, next) {
-  res.render('add', {title: 'Update', message: ""});
+  res.render('index', {title: 'add', message: ""});
   // res.render('submit', { title: 'No' });
 });
 router.post('/Add', upload.array("profile_pic", 2), function (req, res, next) {
-  let _id = req.body.id;
-  let _email = req.body.email;
-  let _diaChi = req.body.diaChi;
-  let _khoa = req.body.khoa;
+  let _id = req.body._id;
+  let _gia = req.body._gia;
+  let _soluong = req.body._soluong;
   let _file = req.files;
   let _namefile = [];
-  // console.log(_file);
   // ==========validate
   if (!_id) {
     const err = new Error("Chua nhap id!");
     return next(err);
   } else {
-    student.findOne({_id: _id}, function (err, data) {
+    xe.findOne({_id: _id}, function (err, data) {
       if (data != null) {
         const err = new Error("ID da ton tai!");
         return next(err);
@@ -165,32 +171,31 @@ router.post('/Add', upload.array("profile_pic", 2), function (req, res, next) {
     })
   }
   //
-  if (!_email) {
+  if (!_gia) {
     const err = new Error("Chua nhap email!");
     return next(err);
   }
-  if (!_diaChi) {
+  if (!_soluong) {
     const err = new Error("Chua nhap dia chi!");
     return next(err);
   }
-  if (!_khoa) {
-    const err = new Error("Chua nhap khoa!");
-    return next(err);
-  }
+
   //
   for (var i = 0; i < _file.length; i++) {
     // console.log("ten: "+_file[i].filename);
     _namefile.push(_file[i].filename);
   }
+
   if (_file.length == 0) {
     const err = new Error("Chua chon file!");
     return next(err);
   } else {
-    const data = new student({
-      _id: _id, _email: _email, _diaChi: _diaChi, _khoa: _khoa, _image: _namefile
+    const data = new xe({
+      _id: _id, _gia: _gia, _soluong: _soluong, _image: _namefile
     });
     data.save(function (err) {
       if (err == null) {
+        console.log("thanh cong add")
         // res.render('index', {title: 'ListAll', message: mess});
         res.redirect("/List");
       } else {
@@ -202,7 +207,7 @@ router.post('/Add', upload.array("profile_pic", 2), function (req, res, next) {
 });
 router.post("/Find", function (req, res,) {
   let _id = req.body.timKiem;
-  student.findOne({_id: _id}, function (err, data) {
+  xe.findOne({_id: _id}, function (err, data) {
     if (err == null) {
       if (data == null) {
         res.render("Find", {title: "Find", data: null, message: "K tim thay!"});
